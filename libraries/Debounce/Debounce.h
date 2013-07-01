85ee984356b615aecd9ea0852d2dacae57e5bbc3
Robert Bakker <r.bakker828@gmail.com>
2013-07-01 20:47:29 +0100

    Fixed comment box

--- libraries/Debounce/Debounce.h
+++ libraries/Debounce/Debounce.h
@@ -1,18 +1,18 @@
 
-/*****************************************************************************************/
-/* Button debouncer for Arduino
-/*****************************************************************************************/
-/* Copyright (c) Robert Bakker 2013
-/*
-/* This file is free software; you can redistribute it and/or modify
-/* it under the terms of either the GNU General Public License version 2
-/* or the GNU Lesser General Public License version 2.1, both as
-/* published by the Free Software Foundation.
-/*****************************************************************************************/
-/* This library handles polling and debouncing of a button.
-/* It is non-blocking code that executes in the background while your sketch
-/* does it's thing.
-/*****************************************************************************************/
+/******************************************************************************************
+ * Button debouncer for Arduino
+ ******************************************************************************************
+ * Copyright (c) Robert Bakker 2013
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ ******************************************************************************************
+ * This library handles polling and debouncing of a button.
+ * It is non-blocking code that executes in the background while your sketch
+ * does it's thing.
+ *****************************************************************************************/
 
 #ifndef Debounce_h		// Include guard
 #define Debounce_h
